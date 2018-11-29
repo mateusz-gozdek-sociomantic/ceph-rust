57714ed4b416fc265d57298c3e939429c9875727
Mateusz Gozdek <mateusz.gozdek@sociomantic.com>
2018-11-29 09:26:06 +0100

    test

--- src/ceph.rs
+++ src/ceph.rs
@@ -2226,18 +2226,19 @@ impl IoCtx {
         &self,
         object_name: &str,
         completion: &mut Completion,
-        fill_buffer: &mut Vec<u8>,
+        //fill_buffer: &mut Vec<u8>,
+        fill_buffer: &mut [u8; 9463],
         read_offset: u64,
     ) -> RadosResult<i32> {
         self.ioctx_guard()?;
         let object_name_str = try!(CString::new(object_name));
-        let mut len = fill_buffer.capacity();
-        println!("Buffer capacity: {}", len);
-        if len == 0 {
-            fill_buffer.reserve_exact(1024 * 64);
-            len = fill_buffer.capacity();
-            println!("New buffer capacity: {}", len);
-        }
+        //let mut len = fill_buffer.capacity();
+        //println!("Buffer capacity: {}", len);
+        //if len == 0 {
+        //    fill_buffer.reserve_exact(1024 * 64);
+        //    len = fill_buffer.capacity();
+        //    println!("New buffer capacity: {}", len);
+        //}
 
         unsafe {
             let ret_code = rados_aio_read(
@@ -2245,7 +2246,8 @@ impl IoCtx {
                 object_name_str.as_ptr(),
                 completion.completion,
                 fill_buffer.as_mut_ptr() as *mut c_char,
-                len as size_t,
+                //len as size_t,
+                9463 as size_t,
                 read_offset,
             );
             println!("rados_aio_read returned {}", ret_code);
